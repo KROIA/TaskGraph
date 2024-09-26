a81646f1d16de6588c8392008248a0edf690e042
Alex <alexkrieg@gmx.ch>
2024-09-26 18:56:53 +0200

    -

--- core/inc/TaskGraph_debug.h
+++ core/inc/TaskGraph_debug.h
@@ -1,8 +1,12 @@
 #pragma once
 #include "TaskGraph_global.h"
 
+/// USER_SECTION_START 14
+
+/// USER_SECTION_END
+
 // The Logger library is automaticly included if the logger dependency .cmake file is available
-#if defined(LOGGER) and not defined(LOGGER_LIB)
+#if defined(LOGGER)
 	#include "Logger.h"
 #endif
 
@@ -107,7 +111,7 @@ namespace TaskGraph
 /// USER_SECTION_END
 
 
-#if defined(LOGGER) and not defined(LOGGER_LIB)
+#if defined(LOGGER)
 	class TASK_GRAPH_EXPORT Logger 
 	{
 		/// USER_SECTION_START 8
@@ -161,6 +165,9 @@ namespace TaskGraph
 
 /// USER_SECTION_END
 #endif
+/// USER_SECTION_START 13
+
+/// USER_SECTION_END
 }
 
 
