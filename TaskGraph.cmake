2854b5247cd3ae662bb19a6d3ffc05623d54115b
Alex Krieg <alexkrieg@gmx.ch>
2024-10-24 11:12:02 +0200

    -

--- TaskGraph.cmake
+++ TaskGraph.cmake
@@ -4,7 +4,7 @@ include(FetchContent)
 function(dep LIBRARY_MACRO_NAME SHARED_LIB STATIC_LIB STATIC_PROFILE_LIB)
     # Define the git repository and tag to download from
     set(LIB_NAME TaskGraph)								
-    set(LIB_MACRO_NAME TASK_GRAPH)						
+    set(LIB_MACRO_NAME TASK_GRAPH_LIBRARY_AVAILABLE)						
     set(GIT_REPO https://github.com/KROIA/TaskGraph.git)
     set(GIT_TAG main)									
 
